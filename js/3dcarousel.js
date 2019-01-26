1faa054648bfd7d91a4b543c94feaad21a665e99
Samantha Lee <lyenchen@yahoo.com>
2019-01-26 19:28:50 +0800

    20190126

--- js/3dcarousel.js
+++ js/3dcarousel.js
@@ -1,12 +1,12 @@
 var ThreeDCarousel = function (el, classname) {
   var element = document.getElementById(el);
   var items = element.getElementsByClassName(classname);
-  var classNames = ['one', 'two', 'three', 'four'];
-  if (items.length !== 4) {
-    alert('ThreeDCarousel only supports 4 items.');
+  var classNames = ['one', 'two', 'three'];
+  if (items.length !== 3) {
+    alert('ThreeDCarousel only supports 3 items.');
     return false;
   } else {
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 3; i++) {
       items[i].className += " " + classNames[i];
     }
   }
@@ -18,21 +18,17 @@ var ThreeDCarousel = function (el, classname) {
       var l = this.element.getElementsByClassName('one')[0];
       var c = this.element.getElementsByClassName('two')[0];
       var r = this.element.getElementsByClassName('three')[0];
-      var q = this.element.getElementsByClassName('four')[0];
       l.classList.remove('one');
       l.classList.add('two');
       c.classList.remove('two');
       c.classList.add('three');
       r.classList.remove('three');
       r.classList.add('one');
-      q.classList.remove('one');
-      q.classList.add('four');
     },
     next: function () {
       var l = this.element.getElementsByClassName('one')[0];
       var c = this.element.getElementsByClassName('two')[0];
       var r = this.element.getElementsByClassName('three')[0];
-      var q = this.element.getElementsByClassName('four')[0];
       l.classList.remove('one');
       l.classList.add('three');
       c.classList.remove('two');
